25830795cd8cbafca5f18e2ddd2541d057e65d4a
seungho <50465901+csh9066@users.noreply.github.com>
2020-10-09 15:19:28 +0900

    Setup typeorm

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,18 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import session from 'express-session';
+import { createConnection } from 'typeorm';
 
 dotenv.config();
 const app = express();
 app.set('port', process.env.PORT || 3005);
+// console.log(ormconfig);
+
+createConnection()
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((e) => console.log(e));
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -25,5 +33,5 @@ app.use(
 );
 
 app.listen(app.get('port'), () => {
-  console.log(`${app.get('port')}port에서 서버 실행 중`);
+  console.log(`${app.get('port')} port에서 서버 실행 중`);
 });
