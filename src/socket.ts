d8195780c366ea22bd68740e1119c48fab2112ff
seungho <50465901+csh9066@users.noreply.github.com>
2020-11-24 17:33:27 +0900

    Update socket

--- src/socket.ts
+++ src/socket.ts
@@ -1,10 +1,21 @@
 import { Application } from 'express';
 import http from 'http';
 import { Server, Socket } from 'socket.io';
+import Channel from './entity/Channel';
 
 export interface IOnlineMap {
   [userId: string]: string;
 }
+
+export const SocketEvent = {
+  ONLINE: 'ONLINE' as const,
+  DM: 'DM' as const,
+  CHANNEL_CHAT: 'CHANNEL_CHAT' as const,
+  JOIN_CHANNELS: 'JOIN_CHANNELS' as const,
+  JOIN_CHANNEL: 'JOIN_CHANNEL' as const,
+  ADD_CHANNEL_MEMBERS: 'ADD_CHANNEL_MEMBERS' as const,
+  ADD_CHANNEL: 'ADD_CHANNEL' as const,
+};
 
 const onlineMap: IOnlineMap = {};
 
@@ -14,22 +25,27 @@ const initialSocket = (appServer: http.Server, app: Application) => {
       origin: '*',
     },
   });
+
   app.set('onlineMap', onlineMap);
   app.set('io', io);
 
   io.on('connection', (socket: Socket) => {
-    console.log('connected user: ', onlineMap);
-
-    socket.on('login', (userId: string) => {
-      // onlineMap[userId] = socket.id
+    socket.on(SocketEvent.ONLINE, (userId: number) => {
       onlineMap[userId] = socket.id;
     });
 
-    socket.on('disconnect', () => {
-      Object.keys(onlineMap).forEach((userId) => {
-        if (onlineMap[userId] == socket.id) {
-          delete onlineMap[userId];
-          console.log('disconnect', socket.id);
+    socket.on(SocketEvent.JOIN_CHANNELS, (channelIds: number[] = []) => {
+      socket.join(channelIds.map((v) => String(v)));
+    });
+
+    socket.on(SocketEvent.JOIN_CHANNEL, (channlId: number) => {
+      socket.join(String(channlId));
+    });
+
+    socket.on('disconnect', async () => {
+      Object.keys(onlineMap).forEach((id) => {
+        if (onlineMap[id] == socket.id) {
+          delete onlineMap[id];
         }
       });
     });
